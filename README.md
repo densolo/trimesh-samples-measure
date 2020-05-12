b78b97e4ff8b205345e8c9ba6cc0d43fd3afb48d
Denis <denis@iMac-Denis.local>
2020-05-12 15:55:06 +0300

    Better calculation of rotation angles

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# Overview
+
+This script parses *.3mf file of a 4x4 array of flat samples and calculates max thickness of each sample.
+
+Orginal file:
+![](images/sample-step1.png)
+
+Rotated:
+![](images/sample-step2.png)
+
+Splited into samples:
+![](images/sample-step3.png)
+
+Thickness of samples:
+```
+detail 01 height 0.927
+detail 02 height 0.505
+detail 03 height 1.191
+detail 04 height 0.454
+detail 05 height 0.859
+detail 06 height 0.681
+detail 07 height 1.096
+detail 08 height 0.787
+detail 09 height 0.908
+detail 10 height 0.888
+detail 11 height 1.278
+detail 12 height 1.145
+detail 13 height 0.579
+detail 14 height 0.614
+detail 15 height 0.510
+detail 16 height 1.002
+```
--- demo1.py
+++ demo1.py
@@ -98,6 +98,28 @@ def split_point_buckets(points, dim, dist):
     return buckets
 
 
+def split_samples(points):
+    xy_samples = []
+    y_buckets = split_point_buckets(points, Y_AXIS, 0.5)
+    for ypoints in y_buckets:
+        xy_samples.extend(split_point_buckets(ypoints, X_AXIS, 0.5))
+
+    print("Found {} samples".format(len(xy_samples)))
+    y_rows = len(y_buckets)
+    x_rows = len(xy_samples)//y_rows
+
+    return xy_samples, x_rows, y_rows
+
+
+def cals_lows(xy_samples):
+    xy_lows = []
+    for i in range(len(xy_samples)):
+        spoints = xy_samples[i]
+        low = min(spoints, key=lambda p: p[Z_AXIS])
+        xy_lows.insert(i, low)
+    return xy_lows
+
+
 def xcol(points):
     return [p[X_AXIS] for p in points]
 
@@ -117,6 +139,41 @@ for t in mesh.triangles: # [:5000]:
         x_arr.append(p[X_AXIS])
         y_arr.append(p[Y_AXIS])
         z_arr.append(p[Z_AXIS])
+
+# Figure Origin
+
+fig = plt.figure(figsize=(20, 10))
+ax1 = plt.subplot(221, projection='3d')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.set_zlabel('Z')
+ax1.view_init(elev=45, azim=45)
+ax1.scatter(x_arr, y_arr, z_arr, s=0.01, c='gray')
+
+ax1 = plt.subplot(222, projection='3d')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.set_zlabel('Z')
+ax1.view_init(elev=0, azim=90)
+ax1.scatter(x_arr, y_arr, z_arr, s=0.01, c='gray')
+
+ax1 = plt.subplot(223, projection='3d')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.set_zlabel('Z')
+ax1.view_init(elev=0, azim=0)
+ax1.scatter(x_arr, y_arr, z_arr, s=0.01, c='gray')
+
+ax1 = plt.subplot(224, projection='3d')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.set_zlabel('Z')
+ax1.view_init(elev=90, azim=0)
+ax1.scatter(x_arr, y_arr, z_arr, s=0.01, c='gray')
+
+# plt.show()
+# exit()
+
 
 z_arr = [-z for z in z_arr]
 
@@ -142,35 +199,65 @@ rotate_xy_rad(x_arr, y_arr, atan(corner_point[Y_AXIS]/corner_point[X_AXIS]))
 #z_arr = [-z for z in z_arr]
 all_points = [[x_arr[i], y_arr[i], z_arr[i]] for i in range(len(x_arr))]
 
-xy_samples = []
-y_buckets = split_point_buckets(all_points, Y_AXIS, 0.5)
-for ypoints in y_buckets:
-    xy_samples.extend(split_point_buckets(ypoints, X_AXIS, 0.5))
-print("Found {} samples".format(len(xy_samples)))
-rows = int(sqrt(len(xy_samples)))
-y_rows = len(y_buckets)
-x_rows = len(xy_samples)//y_rows
+xy_samples, x_rows, y_rows = split_samples(all_points)
+xy_lows = cals_lows(xy_samples)
 
-for i in range(rows):
-    x_samples = xy_samples[i*4:(i+1)*4]
-    #edges = x_samples
-    x_lows = [min(points, key=lambda p: p[Z_AXIS]) for points in x_samples]
-    edges = x_lows
+x_angles = []
+for i in range(x_rows):
+    x_lows = xy_lows[i*4:(i+1)*4]
     arc = np.polyfit(xcol(x_lows), zcol(x_lows), 1)
-    r = atan(np.polyval(arc, 1))
-    print("{} row angle: {} ({})".format(i, r*180/pi, arc))
+    r = atan((np.polyval(arc, 10) - np.polyval(arc, 0))/10)
+    print("X row [{}] angle: {:.2f}".format(i, r*180/pi))
+    x_angles.append(r)
+
+
+# y_angles = []
+# for i in range(y_rows):
+#     y_lows = xy_lows[i::4]
+#     arc = np.polyfit(ycol(y_lows), zcol(y_lows), 1)
+#     r = atan(np.polyval(arc, 1))
+#     print("Y row [{}] angle: {:.2f}".format(i, r*180/pi))
+#     y_angles.append(r)
+
+
+
+
+# # ax1.scatter(xcol(edges), ycol(edges), zcol(edges), s=20, c='red')
+
+# x_arr, y_arr, z_arr = xcol(all_points), ycol(all_points), zcol(all_points)
+# x_lows, y_lows, z_lows = xcol(xy_lows), ycol(xy_lows), zcol(xy_lows)
+# print("x_lows: {}".format(x_lows))
+# print("y_lows: {}".format(y_lows))
+# print("z_lows: {}".format(z_lows))
+# ax1.scatter(x_arr, y_arr, z_arr, s=0.01, c='green')
+# ax1.scatter(x_lows, y_lows, z_lows, s=20, c='red')
+
+# plt.show()
+# exit(0)
+
 
 # rotate Y
-x_lows = [min(points, key=lambda p: p[Z_AXIS]) for points in xy_samples[:4]]
-arc = np.polyfit(xcol(x_lows), zcol(x_lows), 1)
-r = atan((np.polyval(arc, 10) - np.polyval(arc, 0))/10)
+#x_lows = [min(points, key=lambda p: p[Z_AXIS]) for points in xy_samples[:4]]
+#arc = np.polyfit(xcol(x_lows), zcol(x_lows), 1)
+#r = atan((np.polyval(arc, 10) - np.polyval(arc, 0))/10)
+r = sum(x_angles)/len(x_angles)
 rotate_points_xy_rad(all_points, X_AXIS, Z_AXIS, -r)
 
+
+xy_samples, _, _ = split_samples(all_points)
+xy_lows = cals_lows(xy_samples)
+y_angles = []
+for i in range(y_rows):
+    y_lows = xy_lows[i::4]
+    arc = np.polyfit(ycol(y_lows), zcol(y_lows), 1)
+    r = atan((np.polyval(arc, 10) - np.polyval(arc, 0))/10)
+    print("Y row [{}] angle: {:.2f}".format(i, r*180/pi))
+    y_angles.append(r)
+
 # rotate X
-y_lows = [min(points, key=lambda p: p[Z_AXIS]) for points in xy_samples[::4]]
-y_arc = np.polyfit(ycol(y_lows), zcol(y_lows), 1)
-y_r = atan((np.polyval(y_arc, 10) - np.polyval(y_arc, 0))/10)
-rotate_points_xy_rad(all_points, Y_AXIS, Z_AXIS, -y_r)
+r = sum(y_angles)/len(y_angles)
+rotate_points_xy_rad(all_points, Y_AXIS, Z_AXIS, -r)
+
 
 z_low_num = 1000
 
@@ -183,10 +270,7 @@ for p in all_points:
 x_arr, y_arr, z_arr = xcol(all_points), ycol(all_points), zcol(all_points)
 
 
-xy_samples = []
-y_buckets = split_point_buckets(all_points, Y_AXIS, 0.5)
-for ypoints in y_buckets:
-    xy_samples.extend(split_point_buckets(ypoints, X_AXIS, 0.5))
+xy_samples, _, _ = split_samples(all_points)
 
 for i, spoints in enumerate(xy_samples):
     #z_arr = zcol(spoints)
@@ -194,12 +278,14 @@ for i, spoints in enumerate(xy_samples):
     # print(" {} .. {}".format(heights[:5], heights[-5:]))
     low = sum(heights[:100])/100
     high = sum(heights[-5:])/5
-    h = '{} .. {}'.format(['{:.3f}'.format(z) for z in heights[:5]], ['{:.3f}'.format(z) for z in heights[-5:]])
-    print("detail {:02d} height {:.3f}: {}".format(i+1, high-low, h))
+    h = '{} .. {}'.format(['{:+.3f}'.format(z) for z in heights[:5]], ['{:+.3f}'.format(z) for z in heights[-5:]])
+    print("sample {:02d} thickness {:.3f}: {}".format(i+1, high-low, h))
 
 # points = xy_samples[-4] + xy_samples[-3] + xy_samples[-2] + xy_samples[-1]
 x_arr, y_arr, z_arr = xcol(all_points), ycol(all_points), zcol(all_points)
 
+
+# Figure Rotated
 
 fig = plt.figure(figsize=(20, 10))
 ax1 = plt.subplot(111, projection='3d')
@@ -210,8 +296,6 @@ ax1.view_init(elev=35, azim=15)
 ax1.set_xlim(40, -40)
 ax1.set_ylim(-40, 40)
 ax1.set_zlim(-1, 5)
-#ax1.scatter(x_arr, y_arr, z_arr, s=0.01, c='grey')
-ax1.scatter(xcol(edges), ycol(edges), zcol(edges), s=20, c='red')
 
 points = [p for p in all_points if p[Z_AXIS] >= 0]
 x_arr, y_arr, z_arr = xcol(points), ycol(points), zcol(points)
@@ -225,168 +309,83 @@ ax1.scatter(x_arr, y_arr, z_arr, s=0.01, c='cyan')
 points = xy_samples[-4] + xy_samples[-3] + xy_samples[-2] + xy_samples[-1]
 x_arr, y_arr, z_arr = xcol(points), ycol(points), zcol(points)
 
+# # Figure 2
+
+# fig = plt.figure(figsize=(20, 10))
+# gs = fig.add_gridspec(ncols=2, nrows=2, wspace=0.5)
+
+# ax2 = plt.subplot(gs[0,1])
+# ax2.set_xlim(min(x_arr), max(x_arr))
+# ax2.set_ylim(-0.5, 2.0)
+# ax2.scatter(x_arr, z_arr, s=0.01, c='blue')
+# ax2.scatter(xcol(x_lows), zcol(x_lows), s=20, c='red')
+# ax2.minorticks_on()
+# ax2.grid(which='major')
+# ax2.grid(which='minor', linestyle=':')
+# ax2.set_xlabel('x')
+# ax2.set_ylabel('z')
+
+# x = [-100, 100]
+# ax2.add_line(mlines.Line2D(x, np.polyval(arc, x), lw=1, c='red'))
+# #
+# #ax2.grid(which='minor', alpha=0.2)
+# #ax2.plot([-10, 10], [np.polyval(arc, -10), np.polyval(arc, 10)], '-')
+# # plt.axline(arc, slope=0.25, color="red")
+
+# ax3 = plt.subplot(gs[1,1])
+# ax3.set_xlim(min(y_arr), max(y_arr))
+# ax3.set_ylim(-0.5, 2.0)
+# ax3.scatter(y_arr, z_arr, s=0.01, c='deepskyblue')
+# ax3.scatter(ycol(y_lows), zcol(y_lows), s=20, c='red')
+# ax3.yaxis.set_minor_locator(AutoMinorLocator(20))
+# ax3.minorticks_on()
+# ax3.grid(which='major')
+# ax3.grid(which='minor', linestyle=':')
+# ax3.set_xlabel('y')
+# ax3.set_ylabel('z')
+
+# Figure 3
+
 fig = plt.figure(figsize=(20, 10))
-gs = fig.add_gridspec(ncols=2, nrows=2, wspace=0.5)
+gs = fig.add_gridspec(ncols=2*x_rows, nrows=y_rows, wspace=0.5, hspace=0.5)
 
-ax2 = plt.subplot(gs[0,1])
-ax2.set_xlim(min(x_arr), max(x_arr))
-ax2.set_ylim(-0.5, 2.0)
-ax2.scatter(x_arr, z_arr, s=0.01, c='blue')
-ax2.scatter(xcol(x_lows), zcol(x_lows), s=20, c='red')
-ax2.minorticks_on()
-ax2.grid(which='major')
-ax2.grid(which='minor', linestyle=':')
-ax2.set_xlabel('x')
-ax2.set_ylabel('z')
+def draw_samples(xy_samples, x_color=None, y_color=None):
+    for i in range(x_rows):
+        for j in range(y_rows):
+            points = xy_samples[i + y_rows*j]
+            low_points = [xy_lows[i + y_rows+j]]
+            x_arr, y_arr, z_arr = xcol(points), ycol(points), zcol(points)
 
-x = [-100, 100]
-ax2.add_line(mlines.Line2D(x, np.polyval(arc, x), lw=1, c='red'))
-#
-#ax2.grid(which='minor', alpha=0.2)
-#ax2.plot([-10, 10], [np.polyval(arc, -10), np.polyval(arc, 10)], '-')
-# plt.axline(arc, slope=0.25, color="red")
+            ax_n = plt.subplot(gs[x_rows-i-1, j])
+            ax_n.set_xlim(min(y_arr), max(y_arr))
+            ax_n.set_ylim(-0.1, 1.5)
+            ax_n.minorticks_on()
+            ax_n.grid(which='major', lw=0.5, c='black')
+            ax_n.grid(which='minor', alpha=0.5)
+            ax_n.scatter(y_arr, z_arr, s=0.01, c=y_color or 'blue')
+            ax_n.set_xlabel('y')
+            ax_n.set_ylabel('z')
 
-ax3 = plt.subplot(gs[1,1])
-ax3.set_xlim(min(y_arr), max(y_arr))
-ax3.set_ylim(-0.5, 2.0)
-ax3.scatter(y_arr, z_arr, s=0.01, c='deepskyblue')
-ax3.scatter(ycol(y_lows), zcol(y_lows), s=20, c='red')
-ax3.yaxis.set_minor_locator(AutoMinorLocator(20))
-ax3.minorticks_on()
-ax3.grid(which='major')
-ax3.grid(which='minor', linestyle=':')
-ax3.set_xlabel('y')
-ax3.set_ylabel('z')
+            ax_n.scatter(ycol(low_points), zcol(low_points), s=20, c='red')
 
-#plt.show()
+            ax_n = plt.subplot(gs[j, x_rows+i])
+            ax_n.set_xlim(min(x_arr), max(x_arr))
+            ax_n.set_ylim(-0.1, 1.5)
+            ax_n.minorticks_on()
+            ax_n.grid(which='major', lw=0.5, c='black')
+            ax_n.grid(which='minor', alpha=0.5)
+            ax_n.scatter(x_arr, z_arr, s=0.01, c=x_color or 'deepskyblue')
+            ax_n.set_xlabel('x')
+            ax_n.set_ylabel('z')
 
+            ax_n.scatter(xcol(low_points), zcol(low_points), s=20, c='red')
 
-fig = plt.figure(figsize=(20, 10))
-gs = fig.add_gridspec(ncols=2*x_rows, nrows=y_rows, wspace=0.5)
+draw_samples(xy_samples)
 
-for i in range(x_rows):
-    for j in range(y_rows):
-        points = xy_samples[i + y_rows*j]
-        x_arr, y_arr, z_arr = xcol(points), ycol(points), zcol(points)
+# print("rotate a bit more")
+# rotate_points_xy_rad(all_points, Y_AXIS, Z_AXIS, atan(0.1/35))
 
-        ax_n = plt.subplot(gs[j, i])
-        ax_n.set_xlim(min(x_arr), max(x_arr))
-        ax_n.set_ylim(-0.5, 2.0)
-        ax_n.minorticks_on()
-        ax_n.grid(which='major', lw=0.5, c='black')
-        ax_n.grid(which='minor', alpha=0.5)
-        ax_n.scatter(x_arr, z_arr, s=0.01, c='blue')
-        ax_n.set_xlabel('x')
-        ax_n.set_ylabel('z')
-
-        ax_n = plt.subplot(gs[i, x_rows+j])
-        ax_n.set_xlim(min(y_arr), max(y_arr))
-        ax_n.set_ylim(-0.5, 2.0)
-        ax_n.minorticks_on()
-        ax_n.grid(which='major', lw=0.5, c='black')
-        ax_n.grid(which='minor', alpha=0.5)
-        ax_n.scatter(y_arr, z_arr, s=0.01, c='deepskyblue')
-        ax_n.set_xlabel('y')
-        ax_n.set_ylabel('z')
-
-# ax2 = plt.subplot(222, projection='3d')
-# ax2.view_init(elev=0, azim=0)
-# ax2.set_xlim(-40, 40)
-# ax2.set_ylim(-40, 40)
-# ax2.set_zlim(0, 40)
-# ax2.scatter(x_arr, y_arr, z_arr, s=0.01, c='grey')
-
-# ax3 = plt.subplot(223, projection='3d')
-# ax3.view_init(elev=20, azim=30)
-# ax3.set_xlim(-40, 40)
-# ax3.set_ylim(-40, 40)
-# ax3.set_zlim(0, 40)
-# ax3.scatter(x_arr, y_arr, z_arr, s=0.01, c='grey')
+# xy_samples, _, _ = split_samples(all_points,)
+# draw_samples(xy_samples, y_color='red', x_color='coral')
 
 plt.show()
-
-#move_zero(x_arr)
-# move_zero(y_arr)
-# move_zero(z_arr)
-
-# # rotate_xy(x_arr, y_arr, 45)
-
-# #verts = mesh.triangles[:10]
-# #print(verts)
-# ax1.set_xlabel('X')
-# ax1.set_ylabel('Y')
-# ax1.set_zlabel('Z')
-
-# # ax2.view_init(elev=5, azim=5)
-# # ax2.set_xlabel('X')
-# # ax2.set_ylabel('Y')
-# # ax2.set_zlabel('Z')
-
-# # ax2.set_xlim(0,x_lim[1])
-# # ax2.set_ylim(0,y_lim[1])
-# # ax2.set_zlim(0,z_lim[1])
-
-# # ax1.scatter(x_arr, y_arr, z_arr, s=0.1, c='blue')
-
-# iz_lim = find_limit_indexes(z_arr)
-# i = iz_lim[1]
-# x_dots = [x_arr[i], 0]
-# y_dots = [y_arr[i], 0]
-# z_dots = [z_arr[i], 0]
-
-# ax1.scatter(x_dots, y_dots, z_dots, s=10, c='blue')
-
-# r = -atan(z_arr[i]/y_arr[i])
-# print("r1: {} {}".format(y_dots, z_dots))
-# rotate_xy_rad(y_dots, z_dots, r)
-# print("r2: {} {}".format(y_dots, z_dots))
-# ax1.scatter(x_dots, y_dots, z_dots, s=10, c='blue')
-# rotate_xy_rad(y_arr, z_arr, r)
-# #rotate_xy(x_arr, y_arr, 45)
-
-# #
-
-# iz_lim = find_limit_indexes(z_arr)
-# i = iz_lim[1]
-# x_dots = [x_arr[i]]
-# y_dots = [y_arr[i]]
-# z_dots = [z_arr[i]]
-
-# # ax1.scatter(x_dots, y_dots, z_dots, s=10, c='green')
-
-# r = -atan(z_arr[i]/x_arr[i])
-# rotate_xy_rad(x_dots, z_dots, r)
-# # ax1.scatter(x_dots, y_dots, z_dots, s=10, c='green')
-# rotate_xy_rad(x_arr, z_arr, r)
-# # rotate_xy(x_arr, y_arr, 45)
-
-# #ax1.scatter(x_arr, y_arr, z_arr, s=0.1, c='blue')
-
-# # rotate_xy(x_arr, y_arr, 22)
-
-# # move_zero(x_arr)
-# # move_zero(y_arr)
-# # move_zero(z_arr)
-
-
-# # rotate_xy(y_arr, z_arr, -atan(6/70)*180/pi)
-
-# x_lim = find_limit(x_arr)
-# y_lim = find_limit(y_arr)
-# z_lim = find_limit(z_arr)
-# yz_lim = max(y_lim[1], z_lim[1])
-
-# ax1.view_init(elev=10, azim=40)
-# ax1.set_xlim(*x_lim)
-# ax1.set_ylim(*y_lim)
-# ax1.set_zlim(0, 40)
-
-# #ax1.scatter(x_arr, y_arr, z_arr, s=0.01, c='yellow')
-# rotate_xy(x_arr, y_arr, 24)
-# rotate_xy(x_arr, z_arr, 180)
-
-# z_lim = find_limit(z_arr)
-# print("height: {}".format(z_lim))
-
-# ax1.scatter([0], [10], [0], s=10, c='red')
-
