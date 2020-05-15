1f49fa2b18a2dc27d0b8c12d60a4962c8c519548
Denis <denis@iMac-Denis.local>
2020-05-15 10:33:19 +0300

    Clean old code in tsm_gui.py

--- tsm_gui.py
+++ tsm_gui.py
@@ -3,13 +3,14 @@ import tkinter as tk
 from tkinter import filedialog, Menu, Text, END
 import sys
 
+
 class IORedirector(object):
-    '''A general class for redirecting I/O to this Text widget.'''
     def __init__(self,text_area):
         self.text_area = text_area
 
+
 class StdoutRedirector(IORedirector):
-    '''A class for redirecting stdout to this Text widget.'''
+    
     def write(self,str):
         self.text_area.insert(END, str)
 
@@ -28,6 +29,7 @@ def configure_menu(root):
 
     root.config(menu=menubar) 
 
+
 def configure_textarea(root):
     S = tk.Scrollbar(root)
     T = tk.Text(root, height=40, width=120)
@@ -40,23 +42,9 @@ def configure_textarea(root):
 
 def redirector(root, text, inputStr=""):
     import sys
-    # root = Toplevel()
-    #T = Text(root)
     sys.stdout = StdoutRedirector(T)
     sys.stderr = StdoutRedirector(T)
-    #T.pack()
     text.insert(END, inputStr)
-
-# quote = """HAMLET: To be, or not to be--that is the question:
-# Whether 'tis nobler in the mind to suffer
-# The slings and arrows of outrageous fortune
-# Or to take arms against a sea of troubles
-# And by opposing end them. To die, to sleep--
-# No more--and by a sleep to say we end
-# The heartache, and the thousand natural shocks
-# That flesh is heir to. 'Tis a consummation
-# Devoutly to be wished."""
-# T.insert(tk.END, quote)
 
 
 def handle_open():
@@ -69,18 +57,12 @@ def handle_open():
 
     measure_file_with_images(root.filename)
 
-    #labelDone = tk.Label(root, text= 'Completed')
-    #appCanvas.create_window(150, 200, window=labelDone)
-
 
 root = tk.Tk()
-root.title("3MF Sampls Measure")
+root.title("3MF Samples Measure")
 
 configure_menu(root)
 T = configure_textarea(root)
-
 r = redirector(root, T)
 
-print("Python Location: {}".format(sys.executable))
-print("Python Version: {}".format(sys.version))
 root.mainloop()
